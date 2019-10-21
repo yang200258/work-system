e537a64a821da6e4bbda005701309bf81ffbb78d
杨青青4 <qqing_yang@eking-tech.com>
2019-10-21 09:28:45 +0800

    测试jenkins

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -15,7 +15,7 @@
                 </el-form-item>
                 <el-checkbox v-model="checked" class="remember">记住密码</el-checkbox>
                 <el-form-item style="width:100%;">
-                    <el-button type="primary" style="width:100%;" @click.native.prevent="loginRequest" :loading="logining">登录</el-button>
+                    <el-button type="primary" style="width:100%;" @click.native.prevent="loginRequest" :loading="logining">登录2222222222</el-button>
                 </el-form-item>
             </el-form>
         </article>
